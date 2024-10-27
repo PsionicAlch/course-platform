63f56911a57f7185683994d2a94acaaac18af389
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-10-27 17:52:30 +0200

    Added name/surname to users table Added authorization level to users table Added space for an affiliate link in users table Added unique index on affiliate link in users table Added up migrations for keywords table Added up migrations for tutorials table Added up migrations for tutorials_keywords pivot table Added up migrations for tutorials_likes pivot table Added up migrations for tutorials_favorites table Added down migrations for tutorials_favorites table Added down migrations for tutorials_likes table Added down migrations for tutorials_keywords table Added down migrations for tutorials table Added down migrations for keywords table

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,10 +1,20 @@
 CREATE TABLE IF NOT EXISTS users (
     id TEXT PRIMARY KEY,                                -- ULID as the primary key (stored as TEXT)
+    name TEXT NOT NULL,                                 -- User's name
+    surname TEXT NOT NULL,                              -- User's surname
     email TEXT NOT NULL,                                -- User email (unique and required)
     password TEXT NOT NULL,                             -- Hashed password (required)
+    authorization TEXT NOT NULL,                        -- The authorization level that this user has
+
+    affiliate_code TEXT NOT NULL,                       -- User's affiliate code for discounts
+
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,      -- Creation timestamp
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP       -- Update timestamp
 );
 
 -- Create a unique index on the email column to allow quick lookups by email and ensure uniqueness.
 CREATE UNIQUE INDEX idx_users_email ON users(email);
+
+-- Create a unique index on the affiliate code column to ensure that affiliate codes are unique
+-- and to speed up searches based on the user's affiliate code.
+CREATE UNIQUE INDEX idx_users_affiliate_code ON users(affiliate_code);
---
+++ db/migrations/000003_create_tutorials_table.down.sql
@@ -0,0 +1,19 @@
+DROP INDEX IF EXISTS idx_tutorial_favorites_user_id;
+
+DROP TABLE IF EXISTS tutorials_favorites;
+
+DROP INDEX IF EXISTS idx_tutorials_likes_user_id;
+
+DROP TABLE IF EXISTS tutorials_likes;
+
+DROP INDEX IF EXISTS idx_tutorials_keywords;
+
+DROP TABLE IF EXISTS tutorials_keywords;
+
+DROP INDEX IF EXISTS idx_tutorials_slug;
+
+DROP TABLE IF EXISTS tutorials;
+
+DROP INDEX IF EXISTS idx_keywords_keyword;
+
+DROP TABLE IF EXISTS keywords;
