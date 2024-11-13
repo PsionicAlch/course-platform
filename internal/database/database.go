849422dd37daf22698dcdde61459c3fdb81d99db
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-11-13 12:39:28 +0100

    Removed unnecessary database functions in preparation for new system

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,10 +1,5 @@
 package database
 
-import (
-	"github.com/PsionicAlch/psionicalch-home/internal/database/models"
-	"github.com/PsionicAlch/psionicalch-home/pkg/gatekeeper"
-)
-
 type Database interface {
 	// General database functions.
 	Close() error
@@ -15,23 +10,5 @@ type Database interface {
 	Rollback(steps int) error
 
 	// Authentication functions.
-	GetUserInformation(email string) (*gatekeeper.UserInformation, error)
-	AddUser(email, password string) (string, error)
-	AddToken(token *gatekeeper.Token) error
-	GetToken(token string) (*gatekeeper.Token, error)
 
-	// User based functions.
-	FindUserByEmail(email string) (*models.UserModel, error)
-	FindUserByID(id string) (*models.UserModel, error)
-
-	// Tutorial based functions.
-	FindKeyword(keyword string) (*models.KeywordModel, error)
-	AddKeyword(keyword string) (*models.KeywordModel, error)
-	AddKeywordBulk(keywords []string) ([]*models.KeywordModel, error)
-	GetAllTutorials() ([]*models.TutorialModel, error)
-	GetTutorialBySlug(slug string) (*models.TutorialModel, error)
-	AddNewTutorial(title, slug, description, thumbnailUrl, bannerUrl, content, fileChecksum string, keywords []string) error
-	AddNewTutorialBulk(tutorials []*models.TutorialModel) error
-	UpdateTutorial(id string, tutorial *models.TutorialModel) error
-	UpdateTutorialBulk(tutorials []*models.TutorialModel) error
 }
--- website/content/tutorials.go
+++ website/content/tutorials.go
@@ -1,16 +1,10 @@
 package content
 
 import (
-	"crypto/sha256"
 	"embed"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/PsionicAlch/psionicalch-home/internal/database"
-	"github.com/PsionicAlch/psionicalch-home/internal/database/models"
-	"github.com/PsionicAlch/psionicalch-home/internal/utils"
-	"github.com/adrg/frontmatter"
 )
 
 //go:embed tutorials/*.md
@@ -33,134 +27,134 @@ type TutorialData struct {
 }
 
 func (content *Content) RegisterTutorialsContent(waitGroup *sync.WaitGroup, db database.Database) {
-	defer waitGroup.Done()
+	// defer waitGroup.Done()
 
-	var tutorialsToAdd, tutorialsToUpdate []*models.TutorialModel
+	// var tutorialsToAdd, tutorialsToUpdate []*models.TutorialModel
 
-	timerStart := time.Now()
+	// timerStart := time.Now()
 
-	// Get all tutorial file checksums from database.
-	tutorials, err := db.GetAllTutorials()
-	if err != nil {
-		content.ErrorLog.Fatalf("Failed to read all tutorials from the database: %s\n", err)
-	}
+	// // Get all tutorial file checksums from database.
+	// tutorials, err := db.GetAllTutorials()
+	// if err != nil {
+	// 	content.ErrorLog.Fatalf("Failed to read all tutorials from the database: %s\n", err)
+	// }
 
-	// Read all files from embedded file systems.
-	files, err := tutorialsFS.ReadDir("tutorials")
-	if err != nil {
-		content.ErrorLog.Printf("Failed to read tutorials embedded file system: %s\n", err)
-	}
+	// // Read all files from embedded file systems.
+	// files, err := tutorialsFS.ReadDir("tutorials")
+	// if err != nil {
+	// 	content.ErrorLog.Printf("Failed to read tutorials embedded file system: %s\n", err)
+	// }
 
-	content.InfoLog.Printf("Loading %d tutorials into the database!\n", len(files))
+	// content.InfoLog.Printf("Loading %d tutorials into the database!\n", len(files))
 
-	// Read and parse each file.
-	for _, file := range files {
-		// Just in case there are any folders in the folder skip over them.
-		if file.IsDir() {
-			content.WarningLog.Fatalf("Found a directory in tutorials/%s. Directories are not supported.\n", file.Name())
-			continue
-		}
+	// // Read and parse each file.
+	// for _, file := range files {
+	// 	// Just in case there are any folders in the folder skip over them.
+	// 	if file.IsDir() {
+	// 		content.WarningLog.Fatalf("Found a directory in tutorials/%s. Directories are not supported.\n", file.Name())
+	// 		continue
+	// 	}
 
-		// Read the content of the file into a byte slice.
-		output, err := tutorialsFS.ReadFile("tutorials/" + file.Name())
-		if err != nil {
-			content.ErrorLog.Fatalf("Failed to read \"%s\" from tutorials embedded file system: %s", file.Name(), err)
-		}
+	// 	// Read the content of the file into a byte slice.
+	// 	output, err := tutorialsFS.ReadFile("tutorials/" + file.Name())
+	// 	if err != nil {
+	// 		content.ErrorLog.Fatalf("Failed to read \"%s\" from tutorials embedded file system: %s", file.Name(), err)
+	// 	}
 
-		matter := new(TutorialMatter)
-		tutData := new(TutorialData)
+	// 	matter := new(TutorialMatter)
+	// 	tutData := new(TutorialData)
 
-		// Parse the frontmatter to get the file "metadata".
-		data, err := frontmatter.Parse(strings.NewReader(string(output)), matter)
-		if err != nil {
-			content.ErrorLog.Fatalf("Failed to parse markdown from \"%s\": %s", "tutorials/"+file.Name(), err)
-		}
+	// 	// Parse the frontmatter to get the file "metadata".
+	// 	data, err := frontmatter.Parse(strings.NewReader(string(output)), matter)
+	// 	if err != nil {
+	// 		content.ErrorLog.Fatalf("Failed to parse markdown from \"%s\": %s", "tutorials/"+file.Name(), err)
+	// 	}
 
-		hasher := sha256.New()
-		hasher.Write(output)
-		fileChecksum := hasher.Sum(nil)
+	// 	hasher := sha256.New()
+	// 	hasher.Write(output)
+	// 	fileChecksum := hasher.Sum(nil)
 
-		slugIndex, slugFound := utils.InSliceFunc(matter.Slug, tutorials, func(slug string, tutorial *models.TutorialModel) bool {
-			return tutorial.Slug == slug
-		})
+	// 	slugIndex, slugFound := utils.InSliceFunc(matter.Slug, tutorials, func(slug string, tutorial *models.TutorialModel) bool {
+	// 		return tutorial.Slug == slug
+	// 	})
 
-		_, checksumFound := utils.InSliceFunc(string(fileChecksum), tutorials, func(fileChecksum string, tutorial *models.TutorialModel) bool {
-			return tutorial.FileChecksum == fileChecksum
-		})
+	// 	_, checksumFound := utils.InSliceFunc(string(fileChecksum), tutorials, func(fileChecksum string, tutorial *models.TutorialModel) bool {
+	// 		return tutorial.FileChecksum == fileChecksum
+	// 	})
 
-		if slugFound && checksumFound {
-			continue
-		}
+	// 	if slugFound && checksumFound {
+	// 		continue
+	// 	}
 
-		tutData.TutorialMatter = *matter
-		tutData.Content = string(MarkdownToHTML(data))
+	// 	tutData.TutorialMatter = *matter
+	// 	tutData.Content = string(MarkdownToHTML(data))
 
-		if !slugFound {
-			var keywordModels []*models.KeywordModel
-			for _, keyword := range tutData.Keywords {
-				keywordModel := new(models.KeywordModel)
-				keywordModel.Keyword = keyword
+	// 	if !slugFound {
+	// 		var keywordModels []*models.KeywordModel
+	// 		for _, keyword := range tutData.Keywords {
+	// 			keywordModel := new(models.KeywordModel)
+	// 			keywordModel.Keyword = keyword
 
-				keywordModels = append(keywordModels, keywordModel)
-			}
+	// 			keywordModels = append(keywordModels, keywordModel)
+	// 		}
 
-			tutorialToAdd := new(models.TutorialModel)
+	// 		tutorialToAdd := new(models.TutorialModel)
 
-			tutorialToAdd.Title = tutData.Title
-			tutorialToAdd.Slug = tutData.Slug
-			tutorialToAdd.Description = tutData.Description
-			tutorialToAdd.ThumbnailURL = tutData.ThumbnailURL
-			tutorialToAdd.BannerURL = tutData.BannerURL
-			tutorialToAdd.Content = tutData.Content
-			tutorialToAdd.FileChecksum = string(fileChecksum)
-			tutorialToAdd.Keywords = keywordModels
+	// 		tutorialToAdd.Title = tutData.Title
+	// 		tutorialToAdd.Slug = tutData.Slug
+	// 		tutorialToAdd.Description = tutData.Description
+	// 		tutorialToAdd.ThumbnailURL = tutData.ThumbnailURL
+	// 		tutorialToAdd.BannerURL = tutData.BannerURL
+	// 		tutorialToAdd.Content = tutData.Content
+	// 		tutorialToAdd.FileChecksum = string(fileChecksum)
+	// 		tutorialToAdd.Keywords = keywordModels
 
-			tutorialsToAdd = append(tutorialsToAdd, tutorialToAdd)
+	// 		tutorialsToAdd = append(tutorialsToAdd, tutorialToAdd)
 
-			continue
-		}
+	// 		continue
+	// 	}
 
-		if !checksumFound {
-			var keywordModels []*models.KeywordModel
-			for _, keyword := range tutData.Keywords {
-				keywordModel := new(models.KeywordModel)
-				keywordModel.Keyword = keyword
+	// 	if !checksumFound {
+	// 		var keywordModels []*models.KeywordModel
+	// 		for _, keyword := range tutData.Keywords {
+	// 			keywordModel := new(models.KeywordModel)
+	// 			keywordModel.Keyword = keyword
 
-				keywordModels = append(keywordModels, keywordModel)
-			}
+	// 			keywordModels = append(keywordModels, keywordModel)
+	// 		}
 
-			tutorialToUpdate := new(models.TutorialModel)
-			tutorialToUpdate.ID = tutorials[slugIndex].ID
-			tutorialToUpdate.Title = tutData.Title
-			tutorialToUpdate.Slug = tutData.Slug
-			tutorialToUpdate.Description = tutData.Description
-			tutorialToUpdate.ThumbnailURL = tutData.ThumbnailURL
-			tutorialToUpdate.BannerURL = tutData.BannerURL
-			tutorialToUpdate.Content = tutData.Content
-			tutorialToUpdate.Published = false
-			tutorialToUpdate.AuthorID = tutorials[slugIndex].AuthorID
-			tutorialToUpdate.FileChecksum = string(fileChecksum)
-			tutorialToUpdate.Keywords = keywordModels
+	// 		tutorialToUpdate := new(models.TutorialModel)
+	// 		tutorialToUpdate.ID = tutorials[slugIndex].ID
+	// 		tutorialToUpdate.Title = tutData.Title
+	// 		tutorialToUpdate.Slug = tutData.Slug
+	// 		tutorialToUpdate.Description = tutData.Description
+	// 		tutorialToUpdate.ThumbnailURL = tutData.ThumbnailURL
+	// 		tutorialToUpdate.BannerURL = tutData.BannerURL
+	// 		tutorialToUpdate.Content = tutData.Content
+	// 		tutorialToUpdate.Published = false
+	// 		tutorialToUpdate.AuthorID = tutorials[slugIndex].AuthorID
+	// 		tutorialToUpdate.FileChecksum = string(fileChecksum)
+	// 		tutorialToUpdate.Keywords = keywordModels
 
-			tutorialsToUpdate = append(tutorialsToUpdate, tutorialToUpdate)
+	// 		tutorialsToUpdate = append(tutorialsToUpdate, tutorialToUpdate)
 
-			continue
-		}
-	}
+	// 		continue
+	// 	}
+	// }
 
-	if len(tutorialsToAdd) > 0 {
-		if err := db.AddNewTutorialBulk(tutorialsToAdd); err != nil {
-			content.ErrorLog.Fatalf("Failed to bulk add new tutorials: %s", err)
-		}
-	}
+	// if len(tutorialsToAdd) > 0 {
+	// 	if err := db.AddNewTutorialBulk(tutorialsToAdd); err != nil {
+	// 		content.ErrorLog.Fatalf("Failed to bulk add new tutorials: %s", err)
+	// 	}
+	// }
 
-	if len(tutorialsToUpdate) > 0 {
-		if err := db.UpdateTutorialBulk(tutorialsToUpdate); err != nil {
-			content.ErrorLog.Fatalf("Failed to bulk update tutorials: %s", err)
-		}
-	}
+	// if len(tutorialsToUpdate) > 0 {
+	// 	if err := db.UpdateTutorialBulk(tutorialsToUpdate); err != nil {
+	// 		content.ErrorLog.Fatalf("Failed to bulk update tutorials: %s", err)
+	// 	}
+	// }
 
-	timerEnd := time.Since(timerStart)
+	// timerEnd := time.Since(timerStart)
 
-	content.InfoLog.Printf("Parsed %d tutorials in %s\n", len(files), timerEnd)
+	// content.InfoLog.Printf("Parsed %d tutorials in %s\n", len(files), timerEnd)
 }
