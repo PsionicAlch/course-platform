b3dc6267f3d94fc9e221cc132fb9075a34172eaa
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-11-20 13:28:05 +0100

    Added comments table up and down migrations

---
+++ db/migrations/000005_create_comments_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS comments (
+    id TEXT PRIMARY KEY,
+    content TEXT NOT NULL,
+    user_id TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
