ddf36ba9bb65d2824ac674dad09c4d4209501b49
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2024-11-20 13:32:25 +0100

    Forgot to add a reference to the tutorial on the comments table up migration

--- db/migrations/000005_create_comments_table.up.sql
+++ db/migrations/000005_create_comments_table.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS comments (
     id TEXT PRIMARY KEY,
     content TEXT NOT NULL,
     user_id TEXT NOT NULL,
+    tutorial_id TEXT NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (tutorial_id) REFERENCES tutorials(id) ON DELETE CASCADE
 );
