a1bd769f543341086beaab151bfb6972d55de067
Jean-Jacques <34652519+PsionicAlch@users.noreply.github.com>
2025-01-07 17:52:11 +0100

    Update assets.go

--- website/assets/assets.go
+++ website/assets/assets.go
@@ -7,6 +7,8 @@ import (
 //go:embed css img js
 var AssetFiles embed.FS
 
+// TODO: setup asset storage using cloudflare R2 and CDN
+
 // func RegisterAssetRoutes() http.Handler {
 // 	router := chi.NewRouter()
 // 	router.Handle("/*", http.StripPrefix("/assets/", http.FileServerFS(assets)))
