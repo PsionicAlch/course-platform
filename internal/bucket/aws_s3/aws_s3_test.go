7e0cfcd408cb25f7b37bdfb4a63b1997098dd000
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2025-01-23 16:21:15 +0100

    Added placeholder test for bucket

---
+++ internal/bucket/aws_s3/aws_s3_test.go
@@ -0,0 +1,27 @@
+package awss3
+
+import "testing"
+
+func TestSetupS3Bucket(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestBucketExists(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetFileMetadata(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetAllFiles(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestUploadFileFS(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestDeleteFile(t *testing.T) {
+	// TODO: Implement.
+}
