c9e5a599df5915def45067c887bfc7683cafcc28
Jean-Jacques Strydom <jeanjacques.strydom34@gmail.com>
2025-01-23 16:17:44 +0100

    Added placeholder tests for cache

---
+++ internal/cache/go-cache/go_cache_test.go
@@ -0,0 +1,11 @@
+package gocache
+
+import "testing"
+
+func TestSetupGoCache(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestInvalidateCache(t *testing.T) {
+	// TODO: Implement.
+}
---
+++ internal/cache/go-cache/rss_test.go
@@ -0,0 +1,27 @@
+package gocache
+
+import "testing"
+
+func TestGetGeneralRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetTutorialsRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetTutorialRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetCoursesRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetAuthorTutorialsRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
+
+func TestGetAuthorCoursesRSSFeed(t *testing.T) {
+	// TODO: Implement.
+}
